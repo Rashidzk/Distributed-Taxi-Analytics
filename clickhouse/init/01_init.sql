3b72ed9efecabecfd29ff837ad3a9441a0e51b1c
Rashid <rashid.zarkaly@outlook.com>
2026-05-09 10:09:14 +0300

    Add CSV export for ClickHouse loading

--- clickhouse/init/01_init.sql
+++ clickhouse/init/01_init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS taxi.trips_by_hour
     pickup_hour UInt8,
     trip_count UInt32,
     avg_fare Float64,
-    avg_distance Float64
+    avg_distance Float64,
 )
 ENGINE = MergeTree
 ORDER BY (trip_date, pickup_hour);
